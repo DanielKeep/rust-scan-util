54950a7c1b990434ea7a3bac7cd8be5656f53d03
Daniel Keep <daniel.keep@gmail.com>
2014-10-02 14:10:50 +1000

    Simplified int scanner.

--- src/scanner.rs
+++ src/scanner.rs
@@ -9,18 +9,11 @@ pub trait Scanner<T> {
 impl Scanner<int> for int {
 	fn scan<'a, Tok: Tokenizer, Sp: Whitespace>(cursor: &Cursor<'a, Tok, Sp>) -> Result<(int, Cursor<'a, Tok, Sp>), ScanError> {
 		use std::from_str::FromStr;
-		debug!("int.scan({})", cursor);
 
-		let int_str_end = cursor.tail_str().char_indices()
-			.take_while(|&(_,c)| '0' <= c && c <= '9')
-			.map(|(i,_)| Some(i))
-			.last().unwrap_or(None);
+		let int_str_end = scan_int(cursor.tail_str());
 
 		let int_str_end = match int_str_end {
-			Some(i) => {
-				let ::std::str::CharRange { ch:_, next } = cursor.tail_str().char_range_at(i);
-				next
-			},
+			Some(i) => i,
 			None => {
 				return Err(OtherScanError(format!("expected integer, got `{}`", cursor.tail_str()), cursor.consumed()));
 			}
@@ -28,9 +21,6 @@ impl Scanner<int> for int {
 
 		let int_str = cursor.str_slice_to(int_str_end);
 		let cursor = cursor.slice_from(int_str_end);
-
-		debug!(" - int_str: `{}`", int_str);
-		debug!(" - cursor: {}", cursor);
 
 		FromStr::from_str(int_str)
 			.map(ref |i| Ok((i, cursor.clone())))
@@ -40,4 +30,28 @@ impl Scanner<int> for int {
 	fn scanned_value(self) -> int {
 		self
 	}
+}
+
+fn next_char_at(s: &str, at: uint) -> uint {
+	let ::std::str::CharRange { ch: _, next } = s.char_range_at(at);
+	next
+}
+
+fn scan_uint<'a>(s: &'a str) -> Option<uint> {
+	s.char_indices()
+		.take_while(|&(_,c)| '0' <= c && c <= '9')
+		.map(|(i,_)| Some(next_char_at(s, i)))
+		.last().unwrap_or(None)
+}
+
+fn scan_int<'a>(s: &'a str) -> Option<uint> {
+	if s.len() == 0 { return None }
+
+	let (s, off) = if s.char_at(0) == '-' {
+		(s.slice_from(1), 1)
+	} else {
+		(s, 0)
+	};
+
+	scan_uint(s).map(|end| end+off)
 }
