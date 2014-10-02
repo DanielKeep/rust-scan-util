7b2d6e9172be3d7aa953da8209933cf926530219
Daniel Keep <daniel.keep@gmail.com>
2014-10-02 14:10:58 +1000

    Added tests for int scanner.

--- src/scanner.rs
+++ src/scanner.rs
@@ -55,3 +55,35 @@ fn scan_int<'a>(s: &'a str) -> Option<uint> {
 
 	scan_uint(s).map(|end| end+off)
 }
+
+#[cfg(test)]
+mod test {
+	use Cursor;
+	use ScanError;
+	use super::Scanner;
+	use tokenizer::WordsAndInts;
+	use whitespace::Ignore;
+
+	fn cur<'a>(s: &'a str) -> Cursor<'a, WordsAndInts, Ignore> {
+		Cursor::new(s, WordsAndInts, Ignore)
+	}
+
+	fn scan_a<'a, T: Scanner<T>>(s: &'a str) -> Result<(T, Cursor<'a, WordsAndInts, Ignore>), ScanError> {
+		Scanner::scan(&cur(s))
+	}
+
+	#[test]
+	fn test_int() {
+		assert!(scan_a::<int>("").err().is_some());
+		assert!(scan_a::<int>("0").ok().unwrap().0 == 0);
+		assert!(scan_a::<int>("42").ok().unwrap().0 == 42);
+		assert!(scan_a::<int>("1_234").ok().unwrap().0 == 1);
+		assert!(scan_a::<int>("x").err().is_some());
+		assert!(scan_a::<int>("0x").ok().unwrap().0 == 0);
+		assert!(scan_a::<int>("42x").ok().unwrap().0 == 42);
+		assert!(scan_a::<int>("-").err().is_some());
+		assert!(scan_a::<int>("-0").ok().unwrap().0 == 0);
+		assert!(scan_a::<int>("-42").ok().unwrap().0 == -42);
+		assert!(scan_a::<int>("-1_234").ok().unwrap().0 == -1);
+	}
+}
